ab6961570a5b1ab2a30040ef1d7dc5efccc2e5e9
mkpbd <mostofakamal1990@gmail.com>
2024-05-08 08:43:57 +0600

    frist  repeating problem solved

---
+++ dist/problem-1.js
@@ -0,0 +1,5 @@
+"use strict";
+function repeatString(str, repetNumber) {
+    return str.repeat(repetNumber);
+}
+console.log(repeatString("Hello!", 3));
---
+++ src/problem-1.ts
@@ -0,0 +1,7 @@
+
+function repeatString(str: string, repetNumber: number){
+
+    return str.repeat(repetNumber);
+}
+
+console.log(repeatString("Hello!", 3));
