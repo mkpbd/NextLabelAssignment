e8939ef9725ce62ac0d04a54bd2d3b82826d1132
mkpbd <mostofakamal1990@gmail.com>
2024-05-08 09:21:14 +0600

    Problem  4 solved

---
+++ src/problem-4.ts
@@ -0,0 +1,12 @@
+function reverseArray<T>(arrRevers : T[]) : Array<T> {
+
+        let arrRevers2 =  arrRevers.reverse();
+
+        return arrRevers2;
+
+
+}
+
+
+console.log(reverseArray(["apple", "banana", "cherry"]));
+console.log(reverseArray([10, 20, 30]));
