33997b7b67a2e9ee5006a13bfcc2b1e2856791ca
mostofa kamal <mostofakamal1990@gmail.com>
2024-05-08 09:32:41 +0600

    Delete src directory

--- src/problem-1.ts
+++
@@ -1,7 +0,0 @@
-
-function repeatString(str: string, repetNumber: number){
-
-    return str.repeat(repetNumber);
-}
-
-console.log(repeatString("Hello!", 3));
--- src/problem-2.ts
+++
@@ -1,9 +0,0 @@
-
-function findLargestNumber (arr:number[]) : number{
-
-    const largeNumber =  Math.max(...arr);
-
-    return largeNumber;
-}
-
-console.log(findLargestNumber([10, 5, 8, 20, 3]));
