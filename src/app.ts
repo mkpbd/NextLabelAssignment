94aa62e58b0ec02facc9e5988460b76f6f426a13
mkpbd <mostofakamal1990@gmail.com>
2024-05-21 12:51:41 +0600

    bootstrapping is completed

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,12 @@
 
-import  express from 'express'
+import  express,{Request, Response} from 'express';
+import cors from 'cors';
 const app = express()
-const port = 3000
 
-app.get('/', (req, res) => {
+app.use(express.json());
+app.use(cors());
+
+app.get('/', (req:Request, res:Response) => {
   res.send('Hello World!')
 })
 
--- src/server.ts
+++ src/server.ts
@@ -1,19 +1,17 @@
-import mongoose from "mongoose";
-import app from "./app";
-
-
+import mongoose from 'mongoose';
+import app from './app';
+import config from './app/config/indext';
 
 async function main() {
-    try{
-    await mongoose.connect('mongodb://127.0.0.1:27017/myapp');
-  // await mongoose.connect('mongodb://username:password@host:port/database?options...');
-  app.listen(process.env.PORT, () => {
-    console.log(`Server is Runnnin on  port ${process.env.PORT}`)
-  })
-    }catch(error){
-        console.log(error);
-    }
-    
+  try {
+    //await mongoose.connect('mongodb://127.0.0.1:27017/myapp');
+    // await mongoose.connect('mongodb://username:password@host:port/database?options...');
+    app.listen(config.port, () => {
+      console.log(`Server is Runnnin on  port ${config.port}`);
+    });
+  } catch (error) {
+    console.log(error);
+  }
 }
 
 main();
